7e94f348ec671b63075b9991ff307fa5587ee973
Andy Howell <andrew.howell@virginholidays.com>
2018-11-06 10:36:45 +0000

    starting to update

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+    offset: 100, // start scene after scrolling for 100px
+    duration: 400 // pin the element for 400px of scrolling
+})
