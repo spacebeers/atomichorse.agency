d232411f4e57926c5539fba2c7fbde370f10b82e
Andy Howell <andrew.howell@virginholidays.com>
2020-06-18 15:07:55 +0100

    more pages, more copy and more bits

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,29 @@
+
 var Turbolinks = require("turbolinks")
 Turbolinks.start()
+
+document.addEventListener("turbolinks:load", function() {
+    console.log('ready')
+
+    const stickyElm = document.querySelector('header')
+
+    const observer = new IntersectionObserver(([e]) => e.target.classList.toggle('isStuck', !e.isIntersecting), { threshold: 1.0 })
+
+    observer.observe(stickyElm)
+})
+
+if (document.readyState === "interactive") {
+    const event = document.createEvent("Event");
+    event.initEvent("turbolinks:load", true, true);
+    document.dispatchEvent(event);
+}
+
+// var nav = document.querySelector('#nav');
+// nav.addEventListener('click', (e) => {
+//     header.classList.add('open')
+// })
+
+// var close = document.querySelector('#close');
+// close.addEventListener('click', (e) => {
+//     header.classList.remove('open')
+// })
